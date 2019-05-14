c618691110a6707f0756ce4969996327f13694a6
proelbtn <proelbtn@users.noreply.github.com>
2019-05-14 08:38:30 +0000

    add posts table

---
+++ db/migrations/20190514083148_create_table_posts.sql
@@ -0,0 +1,17 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+CREATE TABLE posts (
+    id INTEGER NOT NULL,
+    uid VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(uid) REFERENCES users(id)
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+
+DROP TABLE IF EXISTS posts;
+
