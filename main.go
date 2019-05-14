004c58ab724c71b2dbd6ff43784fd2e4e5fc0c83
proelbtn <proelbtn@users.noreply.github.com>
2019-05-14 08:01:29 +0000

    implement login endpoint

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 	"net/http"
 )
 
+type User struct {
+	id   string `db:"id"`
+	pass string `db:"pass"`
+	name string `db:"display_name"`
+}
+
 type Template struct {
 	templates *template.Template
 }
@@ -45,6 +51,41 @@ func main() {
 
 	e.GET("/login", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "login", nil)
+	})
+
+	e.POST("/login", func(c echo.Context) error {
+		sess, err := session.Get("session", c)
+		if err != nil {
+			return err
+		}
+
+		params, err := c.FormParams()
+		if err != nil {
+			return err
+		}
+
+		db, err := sql.Open("sqlite3", "file:database.sqlite3")
+		if err != nil {
+			return err
+		}
+
+		id, pass := params.Get("id"), params.Get("pass")
+
+		rows, err := db.Query("SELECT * FROM users WHERE id = ? AND pass = ?", id, pass)
+
+		// TODO: better error handling
+		if err != nil {
+			return c.Render(http.StatusNotAcceptable, "login", nil)
+		}
+
+		var user User
+		if rows.Scan(user) != nil && !rows.Next() {
+			sess.Values["id"] = user.id
+			sess.Save(c.Request(), c.Response())
+			return c.Redirect(http.StatusSeeOther, "/")
+		}
+
+		return c.Render(http.StatusNotAcceptable, "login", nil)
 	})
 
 	e.GET("/signup", func(c echo.Context) error {
